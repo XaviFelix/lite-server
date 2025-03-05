adb1ccd67329a6911b31aeb37d6b9457f9c27530
xfelix <xavierfelix74@gmail.com>
2025-03-04 22:32:20 -0800

    refactored

--- main.go
+++ main.go
@@ -3,29 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/XaviFelix/lite-server.git/handlers"
 )
-
-// Root handler that handles requests to "/"
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world")
-}
-
-// greet handler
-func greetHandler(w http.ResponseWriter, r *http.Request) {
-
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		fmt.Fprintf(w, "Hello there, stranger!")
-	} else {
-		fmt.Fprintf(w, "Hi, %s! welcome to my server!", name)
-	}
-}
 
 func main() {
 
 	// registering my handlers:
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/greet", greetHandler)
+	http.HandleFunc("/", handlers.RootHandler)
+	http.HandleFunc("/greet", handlers.GreetHandler)
 
 	// begin the server on port 8080
 	fmt.Println("Server starting on :8080...")
@@ -33,5 +19,4 @@ func main() {
 	if err != nil {
 		fmt.Println("Error starting server: ", err)
 	}
-
 }
